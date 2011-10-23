d22416c15fd95453266940fc6dea55aba57fcc2c
to <to@ubuntu.(none)>
2011-10-23 16:01:18 +0200

    Hello Bananenbaum

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bananenbaum::Application.config.secret_token = '3dbaa14d4f07e86331b299b62c978b18542faa7606b1a3400184432fe1ac972cd67e005f8594c36abb97f6fff45a54db4ca25b885cfd512652a74f605ecc7bc2'
